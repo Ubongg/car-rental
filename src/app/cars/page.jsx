b9ffe6c87596d22122f54bdf08ffc4417721a07b
Ubongg <ubongeffiong5@gmail.com>
2023-07-04 10:03:04 +0100

    added navbar

---
+++ src/app/cars/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./page.module.css";
+
+const Cars = () => {
+  return <div>Cars</div>;
+};
+
+export default Cars;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState, useRef, useEffect } from "react";
+import styles from "./navbar.module.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About Us",
+    url: "/about",
+  },
+  {
+    id: 3,
+    title: "Cars",
+    url: "/cars",
+  },
+  {
+    id: 4,
+    title: "News",
+    url: "/news",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = "100vh";
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
+  return (
+    <div className={`${showLinks ? styles.navCenterShadow : styles.navCenter}`}>
+      <div className={styles.navHeader}>
+        <Link href="/">
+          <h1 className={styles.logo}>
+            Yubo <br />
+            <span>Rental</span>
+          </h1>
+        </Link>
+        <button
+          className={`${showLinks ? styles.navToggleShow : styles.navToggle}`}
+          onClick={toggleLinks}
+        >
+          {showLinks ? <FaTimes /> : <FaBars />}
+        </button>
+      </div>
+      <div className={styles.linksContainer} ref={linksContainerRef}>
+        <ul className={styles.links} ref={linksRef}>
+          {links.map((link) => (
+            <li>
+              <Link
+                key={link.id}
+                href={link.url}
+                className={styles.link}
+                onClick={toggleLinks}
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Link className={styles.signIn} href="/dashboard/login">
+        Sign In
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
