a981238951ea4fee057d95634dc234836062bdd9
Ubongg <ubongeffiong5@gmail.com>
2023-08-10 23:44:48 +0100

    done with dashboard and single order page

--- src/app/api/orders/[id]/route.js
+++ src/app/api/orders/[id]/route.js
@@ -12,3 +12,17 @@ export const PUT = async (request, { params }) => {
 
   return NextResponse.json({ message: "Order updated" }, { status: 200 });
 };
+
+export const GET = async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    await connect();
+
+    const order = await Order.findById(id);
+
+    return new NextResponse(JSON.stringify(order), { status: 200 });
+  } catch (err) {
+    return new NextResponse("Database Error", { status: 500 });
+  }
+};
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -2,11 +2,13 @@
 
 import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Background from "@/components/background/background";
 import useSWR from "swr";
-import Image from "next/image";
+import { BiUserCircle } from "react-icons/bi";
+import { FaSignOutAlt } from "react-icons/fa";
+import Link from "next/link";
 
 const Dashboard = () => {
   const session = useSession();
@@ -62,49 +64,85 @@ const Dashboard = () => {
     return (
       <div className={styles.dashboard}>
         <Background header="Dashboard" />
-        <div className={styles.scheduled}>
-          <h2>Scheduled Orders</h2>
-          {data?.map((order) => {
-            if (order.status === "scheduled") {
-              return (
-                <div key={order._id}>
-                  <p>{order.carName}</p>
-                  <button onClick={() => completedStatus(order._id)}>
-                    Complete
-                  </button>
-                  <button onClick={() => cancelledStatus(order._id)}>
-                    cancel
-                  </button>
-                </div>
-              );
-            }
-          })}
-        </div>
+        <div className={styles.dashboardWrap}>
+          <div className={styles.profile}>
+            <div className={styles.imgBx}>
+              <BiUserCircle />
+            </div>
+            <div className={styles.textBx}>
+              <h5>{session?.data?.user.name}</h5>
+              <p>{session?.data?.user.email}</p>
+            </div>
+            <button className={styles.signOut} onClick={signOut}>
+              <span className={styles.icon}>
+                <FaSignOutAlt />
+              </span>
+              <span className={styles.text}>Sign Out</span>
+            </button>
+          </div>
+          <div className={styles.orders}>
+            <div className={styles.scheduled}>
+              <h2>Scheduled Orders</h2>
+              {data?.map((order) => {
+                if (order.status === "scheduled") {
+                  return (
+                    <div key={order._id} className={styles.order}>
+                      <p>{order.carName}</p>
+                      <div className={styles.statusButtons}>
+                        <button
+                          onClick={() => completedStatus(order._id)}
+                          className={styles.complete}
+                        >
+                          Complete
+                        </button>
+                        <button
+                          onClick={() => cancelledStatus(order._id)}
+                          className={styles.cancel}
+                        >
+                          cancel
+                        </button>
+                      </div>
+                      <button className={styles.showDetails}>
+                        <Link href={`/dashboard/${order._id}`}>Details</Link>
+                      </button>
+                    </div>
+                  );
+                }
+              })}
+            </div>
 
-        <div className={styles.completed}>
-          <h2>Completed Orders</h2>
-          {data?.map((order) => {
-            if (order.status === "completed") {
-              return (
-                <div key={order._id}>
-                  <p>{order.carName}</p>
-                </div>
-              );
-            }
-          })}
-        </div>
+            <div className={styles.completed}>
+              <h2>Completed Orders</h2>
+              {data?.map((order) => {
+                if (order.status === "completed") {
+                  return (
+                    <div key={order._id} className={styles.order}>
+                      <p>{order.carName}</p>
+                      <button className={styles.showDetails}>
+                        <Link href={`/dashboard/${order._id}`}>Details</Link>
+                      </button>
+                    </div>
+                  );
+                }
+              })}
+            </div>
 
-        <div className={styles.cancelled}>
-          <h2>Cancelled Orders</h2>
-          {data?.map((order) => {
-            if (order.status === "cancelled") {
-              return (
-                <div key={order._id}>
-                  <p>{order.carName}</p>
-                </div>
-              );
-            }
-          })}
+            <div className={styles.cancelled}>
+              <h2>Cancelled Orders</h2>
+              {data?.map((order) => {
+                if (order.status === "cancelled") {
+                  return (
+                    <div key={order._id} className={styles.order}>
+                      <p>{order.carName}</p>
+                      <button className={styles.showDetails}>
+                        <Link href={`/dashboard/${order._id}`}>Details</Link>
+                      </button>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+          </div>
         </div>
       </div>
     );
